9e0956103726853eb9e4f93a290cf93b5c2d4e74
Guillaume <guillaume-cornet07@hotmail.fr>
2021-02-12 16:42:40 +0100

    feat: #16 create routing with mux

--- api/server/server.go
+++ api/server/server.go
@@ -1,1 +1,19 @@
 package server
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type Server struct {
+	Router *mux.Router
+}
+
+func (s *Server) Run() *Server {
+	s.InitialiseRoutes()
+	log.Fatal(http.ListenAndServe(":8080", s.Router))
+
+	return s
+}
+
