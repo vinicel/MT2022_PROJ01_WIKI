e3002eae4eae9a17ddf61508920d34a2f6cdf704
Guillaume <guillaume-cornet07@hotmail.fr>
2021-03-28 16:44:23 +0200

    feat: #24 add validation creating user

--- models/accounts.go
+++ models/accounts.go
@@ -5,6 +5,7 @@ import (
 	u "github.com/Wiki-Go/utils"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
+	"log"
 	"strings"
 	"time"
 )
@@ -44,20 +45,19 @@ func (accounts *Database) Validate() (map[string]interface{}, bool) {
 }
 
 func (accounts *Database) CreateUser() (map[string]interface{}) {
-/*	if resp, ok := accounts.Validate(); !ok {
+	if resp, ok := accounts.Validate(); !ok {
 		return resp
 	}
-*/
+
 	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(accounts.Account.Password), bcrypt.DefaultCost)
 
 	accounts.Account.Password = string(hashedPassword)
 	fmt.Printf("Email: %v\n Password: %v\n Firstname: %v\n Lastname: %v\n", accounts.Account.Email, accounts.Account.Password, accounts.Account.Firstname,accounts.Account.Lastname)
 	accounts.Db.Create(&accounts.Account)
-	/*if err != nil && err != gorm.ErrRecordNotFound {
+	if err != nil && err != gorm.ErrRecordNotFound {
 		log.Fatal(err)
 		return u.Message(false, "Connection error. please retry")
-	}*/
-	//InitGorm().Create(accounts)
+	}
 
 	response := u.Message(true, "Account created")
 	response["account"] = accounts
