ce9064bf1d64c421e783dbe35b111bb945658dfc
MosieurClement <clement.haller@noir.dev>
2021-03-29 09:51:46 +0200

    add documentation

--- controllers/contoller.go
+++ controllers/contoller.go
@@ -1,15 +1,10 @@
 package controllers
 
-import (
-	"encoding/json"
-	"gorm.io/gorm"
-	"net/http"
-)
-
+/*
 type Controller struct {
 	Db		*gorm.DB
-}
-
+}*/
+/*
 func (c *Controller) WriteJson(w http.ResponseWriter, value interface{}) {
 	output, err := json.Marshal(value)
 	if err != nil {
@@ -18,4 +13,4 @@ func (c *Controller) WriteJson(w http.ResponseWriter, value interface{}) {
 	}
 	w.Header().Set("Content-type", "application/json")
 	w.Write(output)
-}
+}*/
