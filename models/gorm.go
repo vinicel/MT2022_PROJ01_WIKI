a63f6a469a6345afdae50b61e1718e7115dcb8ac
MosieurClement <clement.haller@noir.dev>
2021-04-02 22:25:50 +0200

    test

--- models/gorm.go
+++ models/gorm.go
@@ -50,7 +50,7 @@ type ModelInterface interface {
 
 func InitGorm() *gorm.DB {
 	fmt.Println(os.Getenv("DATABASE_URL"))
-	dsn := fmt.Sprintf("user:root@tcp(%v:3306)/wikiGo?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("DATABASE_URL"))
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:3306)/wikiGo?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD") ,os.Getenv("DATABASE_URL"))
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
