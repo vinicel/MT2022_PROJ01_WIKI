fdc04764ccb4b11dc1a28068ff8d0b0efda1d1eb
MosieurClement <clement.haller@noir.dev>
2021-07-30 12:20:43 +0200

    change initdb to main

--- connector/connector.go
+++ connector/connector.go
@@ -5,19 +5,18 @@ import (
 	"github.com/subosito/gotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"log"
 	"os"
 )
 
-var Db *gorm.DB = initDb()
+var Db *gorm.DB
 
-func initDb() *gorm.DB  {
+func InitDb() (*gorm.DB, error)  {
 	gotenv.Load()
 	fmt.Println(os.Getenv("DATABASE_URL"))
 	dsn := fmt.Sprintf("%v:%v@tcp(%v:3306)/wikiGo?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD") ,os.Getenv("DATABASE_URL"))
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal(err)
+		return db, err
 	}
-	return db
+	return db, nil
 }
--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"github.com/vinicel/MT2022_PROJ01_WIKI/connector"
 	server2 "github.com/vinicel/MT2022_PROJ01_WIKI/server"
+	"log"
 )
 
 
 func main()  {
+	var err error
+	connector.Db, err = connector.InitDb()
+	if err != nil {
+		log.Fatal(err)
+	}
 	api := server2.Server{}
 	//api.InitialiseRoutes()
 	api.Run()
